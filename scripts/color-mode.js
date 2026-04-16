0c6fb761d708b98535f9c5b08ea115085e1ba52a
ZJarjur <zjarjur@gmail.com>
2026-04-16 15:49:43 +0800

    Step 6 plus

---
+++ scripts/color-mode.js
@@ -0,0 +1,17 @@
+let button = document.getElementById("color-mode-button");
+
+
+function toggleColorMode(event) {
+    if (document.body.classList.contains("dark-mode")){
+        button.textContent = "Dark Mode";
+        document.body.classList.replace("dark-mode", "light-mode");
+    }else{
+        button.textContent = "Light Mode";
+        document.body.classList.replace("light-mode", "dark-mode");
+    }
+    console.log("event = ", event);
+
+}
+
+button.addEventListener("click", toggleColorMode);
+
